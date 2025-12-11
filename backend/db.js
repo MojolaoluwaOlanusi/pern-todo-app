481e18ef93acdbbab2defd10226d002db08cf53b
MojolaoluwaOlanusi <olanusimojola@gmail.com>
2025-12-11 12:48:41 +0100

    used neon for database

--- backend/db.js
+++ backend/db.js
@@ -1,11 +1,16 @@
-import {Pool} from 'pg';
+import { Pool } from 'pg';
+
+const connectionString = "postgresql://neondb_owner:npg_k0phvPTzGl5E@ep-restless-water-a4nunlxk-pooler.us-east-1.aws.neon.tech/neondb?sslmode=require&channel_binding=require";
+
+if (!connectionString) {
+    throw new Error('No database connection string found. Set NEON_URI or DATABASE_URL in .env');
+}
 
 const pool = new Pool({
-    user: "postgres",
-    password: "Darasimi_2010",
-    host: "localhost",
-    port: 5432,
-    database: "tododb",
+    connectionString,
+    ssl: {
+        rejectUnauthorized: false,
+    },
 });
 
 export default pool;
